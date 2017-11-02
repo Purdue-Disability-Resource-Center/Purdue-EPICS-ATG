8d8dd8f6c1c76594a3cdd94bd2ac3c7b8460601b
Joseph A. Gerardot <indyJAG@gmail.com>
2017-11-02 13:51:50 -0400

    Add JavaDoc comments to MainActivity. Change user input for route selection from Fling to LongPress.

--- app/src/main/java/edu/purdue/engineering/atg/MainActivity.java
+++ app/src/main/java/edu/purdue/engineering/atg/MainActivity.java
@@ -58,7 +58,11 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     private FusedLocationProviderClient locator;
     private LocationRequest locationRequest;
     private GestureDetector gestureDetector;
-
+/** Method for initializing the main activity. Sets up the layout and the statistics manager. Registers itself as a gesture listener
+ *  and as a {@code TexttoSpeech.OnInitListener}. Makes calls to check app permissions and perform appropriate initialization.
+ *  Initializes the {@code TexttoSpeech} speaker for list navigation. 
+ *  @param savedInstanceState {@code Bundle} containing saved instance data. Not used in this code, simply passed into {@code super}.
+ */
     @Override @TargetApi(26)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -100,18 +104,20 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
             requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
         }
     }
-
+/** Initialization of the {@code FileManager}. Not safe to call unless app is known to have external file permissions. */
     private void initFileManager() {
         fileManager = new FileManager(
                 new File(Environment.getExternalStorageDirectory().getPath()+ "/" + ROUTES_DIRECTORY) //should be the directory with routes
         );
     }
-
+/** Called when the app needs to start up processes before coming onscreen. Simply calls {@code super}. */
     protected void onStart() {
         super.onStart();
 
     }
-
+/** Called immediately when app becomes active onscreen. Sets the {@code isInForeground} flag true and sets the current onscreen route, so the
+ *  description will play. Calls the GPS to start, and the {@code startGPS()} method handles permissions.
+ */
     protected void onResume() {
         super.onResume();
         startGPS();
@@ -120,14 +126,14 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
             setCurrentRoute(fileManager.getRoute());
 
     }
-
+/** Called immediately when app is covered. Simply stops the GPS and turns off the {@code isInForeGround} flag. */
     protected void onPause() {
         super.onPause();
         stopGPS();
         isInForeground = false;
 
     }
-
+/** Called when app goes fully offscreen. Simply calls {@ super.onStop()} */
     protected void onStop() {
        super.onStop();             // I don't think we need to do anything in here
     }
@@ -135,7 +141,13 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     protected void onDestroy() {
         super.onDestroy();
     }
-
+/** Handler for when the permissions request comes back from user. Uses a switch case to figure out which permission was questioned and 
+ *  whether it was granted. If true, it calls the appropriate initializations. This does mean that if the app starts without permission, it
+ *  will perform these processes asynchronously instead of synchronously. 
+ *  @param requestCode The request code passed by the app when it made the request. Used to determine what we asked for.
+ *  @param permissions Array containing the permissions we asked. Not used in this method.
+ *  @param grantResults Array with the {@code PackageManager} success values from the user. We only check the first element here.
+ */
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch(requestCode) {
             case(MY_PERMISSIONS_REQUEST_READ_FINE_LOCATION):
@@ -164,7 +176,10 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
                 break;
         }
     }
-
+/** Starts the GPS functionality of the main screen. Has its own check for permissions, so it can simply be called without regard. Will
+ *  not do anything if the GPS is already registered, or if permissions are not enabled. 
+ *  @see #MainActivity.stopGPS()
+ */
     private void startGPS() {
         if(location_permissions_ready && !requestingLocationUpdates) {
               //check if we somehow got here while already requesting location updates
@@ -178,7 +193,9 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
         }
     }
-
+/** Stops GPS operation. Has similar checks to {@code startGPS()}.
+ *  @see #MainActivity.startGPS()
+ */
     private void stopGPS() {
         if(location_permissions_ready && requestingLocationUpdates) {
             stats.updateGPSState(StatsManager.GPS_INACTIVE); //these lines set readout and boolean to reflect state
@@ -186,7 +203,8 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
             locator.removeLocationUpdates(stats.getLocationCallback()); //take off the request
         }
     }
-
+/** Initializes everything related to location services and the GPS. Called upon confirmation of permissions, exactly once per app instance.
+ */
     private void initLocationServices() {
         locator = LocationServices.getFusedLocationProviderClient(this);
 
@@ -226,14 +244,19 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         });
 
     }
-
+/** Throws user out of MainActivty and into navigation on the given route. Called upon receipt of whatever UI interaction translates to
+ *  "select route." 
+ *  @param ptr The {@code RoutePtr} representing the route on which to start. In this program passed out of {@code FileManager}.
+ */
     private void beginRoute(RoutePtr ptr) {
         startActivity( new Intent(this, RouteNavigateLaunch.class)
                 .putExtra("stats", stats)
                 .putExtra("route", ptr)
         );
     }
-
+/** Plays the description of a given Route. This is used to read off the route to the visually-impaired user.
+ *  @param ptr The route to be described.
+ */
     private void playDesc(RoutePtr ptr) {
         Uri content = ptr.getDesc();
         File file = new File(content.getPath());
@@ -269,17 +292,23 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
                 }
         }
     }
-
+/** Sets the current displayed route to the given RoutePtr. Call whenever the current route needs to be updated to the user.
+ *  @param route The new current route. Will be displayed on screen and have it's description played.
+ */
     private void setCurrentRoute(RoutePtr route) {
         routeMenu.setText(R.string.route_menu_text + route.getName());
         playDesc(route);
     }
-
+/** Go to the next route and set the current route to the new route. */
     private void nextRoute() {
         setCurrentRoute(fileManager.proceedRoute());
     }
 
     /*-------------------------- Gesture Handlers -----------------------------*/
+/** Handler for when app recieves any {@code TouchEvent}. Immediately passes to {@code GestureDetector}
+ *  @param e The {@code MotionEvent} to be handled.
+ *  @return A boolean informing the handler whether the event is done.
+ */
     public boolean onTouchEvent(MotionEvent e) {
         gestureDetector.onTouchEvent(e);
         return super.onTouchEvent(e);
@@ -289,12 +318,14 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     }
 
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityx, float velocityy) {
-        beginRoute(fileManager.getRoute());
         return true;
     }
-
+/** {@code GestureDetector} handler for a long press. Indicates selection of the current route. Calls {@link beginRoute(RoutePtr) beginRoute}
+ *  @param e The {@code MotionEvent} representing the input. Not actually used in this method.
+ *  @see #beginRoute(RoutePtr)
+ */
     public void onLongPress(MotionEvent e) {
-
+        beginRoute(fileManager.getRoute());
     }
 
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distancex, float distancey) {
@@ -304,14 +335,17 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     public void onShowPress(MotionEvent e) {
 
     }
-
+/** Handler for when a single tap is received. Indicates the user would like to move to the next route in the list. 
+ *  Calls {@link #nextRoute() nextRoute()}. 
+ *  @see #nextRoute()
+ */
     public boolean onSingleTapUp(MotionEvent e) {
         nextRoute();
         return true;
     }
 
     //-------------------------- TexttoSpeech Interface ----------------------------
-
+/** Handler for when the TexttoSpeech initialization returns. Sets the flag to ready so the other operations can proceed without issue. */
     public void onInit(int status) {
         if(status == TextToSpeech.SUCCESS)
             speaker_ready = true;
